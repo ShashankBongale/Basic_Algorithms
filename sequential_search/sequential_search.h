b2992e583619176ea0f7d62aab5aad9edb715dc1
Shashank_Bongale <shashankbongale@gmail.com>
2018-08-01 10:01:11 +0530

    Make files added for all pgm

--- sequential_search/sequential_search.h
+++
@@ -1,4 +0,0 @@
-//this is the header file for our sequentional search program
-//function to find the first occurance
-//we are passing array refernce,number of elements in array and key to be searched
-int sequential_search(int *arr,int n,int k);
